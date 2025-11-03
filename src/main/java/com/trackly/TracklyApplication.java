ddc7f76c7ff083bbe0ab056c84721c5ca4426d24
Chiara <chiara-zuc@libero.it>
2025-11-03 22:37:27 +0100

    print envs

--- src/main/java/com/trackly/TracklyApplication.java
+++ src/main/java/com/trackly/TracklyApplication.java
@@ -8,7 +8,12 @@ import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServic
 public class TracklyApplication {
 
 	public static void main(String[] args) {
+		System.out.println("DB_HOST = " + System.getenv("DB_HOST"));
+		System.out.println("DB_PORT = " + System.getenv("DB_PORT"));
+		System.out.println("DB_NAME = " + System.getenv("DB_NAME"));
+		System.out.println("DB_USER = " + System.getenv("DB_USER"));
+		System.out.println("DB_PASSWORD = " + System.getenv("DB_PASSWORD"));
+
 		SpringApplication.run(TracklyApplication.class, args);
 	}
-
 }
